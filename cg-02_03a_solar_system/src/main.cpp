69ed871b11e2d47d28334ea737bfa556b165a634
sosterwalder <sven.osterwalder@gmail.com>
2013-11-08 22:36:08 +0100

    * Removed GL core profile stuff.. doesn't seem to work within linux

--- cg-02_03a_solar_system/src/main.cpp
+++ cg-02_03a_solar_system/src/main.cpp
@@ -48,11 +48,10 @@ class Framework
 
             printf("INFO: Initialized GLFW\n");
 
-
-            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-            glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-            glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+            //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+            //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+            //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+            //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
             glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
             /* Create a windowed mode window and its OpenGL context */
